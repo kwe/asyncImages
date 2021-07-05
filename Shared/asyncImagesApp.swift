a59dadda3cc9907db0d7cab82691f5f7d93b9b95
kwe <kwevans@gmail.com>
2021-07-05 21:09:37 +0100

    Initial Commit

---
+++ Shared/asyncImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncImagesApp.swift
+//  Shared
+//
+//  Created by Kevin Evans on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct asyncImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
